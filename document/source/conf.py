ea88e3335d61548f2f5fa8f5c9d9b1fdbbe8dfa3
AllDataFounder <m13602574132@163.com>
2025-01-19 18:51:31 +0800

    [Feature][document]数据中台开源项目文档版本号修改为0.6.x

--- document/source/conf.py
+++ document/source/conf.py
@@ -9,7 +9,7 @@
 project = 'AllData'
 copyright = '2023, AllDataDC'
 author = 'AllDataDC'
-release = '0.6.1'
+release = '0.6.x'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- document/source/index.rst
+++ document/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-AllData数据中台文档 v0.5.2
+AllData数据中台文档 v0.6.x
 ===================================
 
 .. toctree::
